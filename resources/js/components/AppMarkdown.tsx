6f4b9bbacdc7b49ec693dad19d69f0617d8dfa1b
Shibaji Debnath <imshibaji@gmail.com>
2025-07-22 01:08:40 +0530

    QA Bug Fixes

--- resources/js/components/AppMarkdown.tsx
+++ resources/js/components/AppMarkdown.tsx
@@ -3,20 +3,12 @@ import Markdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Or any other style
 
-export interface CodeProps {
-  inline?: boolean;
-  node?: any;
-  className?: string;
-  children?: React.ReactNode;
-  [key: string]: any;
-}
-
 
 export default function AppMarkdown({content}: {content?: React.ReactNode}) {
       return (
         <Markdown
           components={{
-            code({ node, inline, className, children, ...props }: CodeProps) {
+            code({ node, inline, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
                 <SyntaxHighlighter
